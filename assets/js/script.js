e1d708885814412ac536991b0639831a4c0f4010
Adam-Daly <adamacus+github@gmail.com>
2023-01-26 10:12:22 +0000

    added javascript framework and initial variables for categories and hint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
-console.log("Connected!")
+// Wait for dom to be loaded before fetching elements
+document.addEventListener('DOMContentLoaded', function () {
+
+//Define variables
+let category;
+
+//Define categories and hints
+let categories = {
+    "Video Games": ["Minecraft", "Fortnite", "Overwatch"],
+    "Countries": ["Australia", "Japan", "Egypt"],
+    "Social Media Sites": ["Instagram", "Twitter", "Tiktok"]
+};
+
+let hints = {
+    "Minecraft": "A popular sandbox game where you build with blocks",
+    "Fortnite": "A popular battle royale from Epic Games",
+    "Overwatch": "A competitive team hero shooter from Blizzard",
+    "Australia": "Country known for their Kangaroos",
+    "Japan": "Country known for their sushi",
+    "Egypt": "Country known for their ancient pyramids",
+    "Instagram": "Social media site known for photography",
+    "Twitter": "Social media site known for microblogging and tight character limits",
+    "Tiktok": "Social media site known for sharing short video clips"
+};
+
+//Make category buttons
+
+//Pick a random word from the chosen category
+
+//Make alphabet buttons for guessing letters
+
+//Check letter against word by rebuilding the word through comparison
+
+//Make restart feature and button
+
+});
